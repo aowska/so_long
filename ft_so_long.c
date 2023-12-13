b55f14c77c38e8f1b432fdb7d186cd3657e7da8e
Alicja Wacowska <awacowsk@c4c1c1.42berlin.de>
2023-12-13 21:27:29 +0100

    initial

--- ft_so_long.c
+++ ft_so_long.c
@@ -10,44 +10,85 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h> // Dodaj brakujący nagłówek dla funkcji malloc i free
-#include "./ft_so_long.h"
+// Dodaj brakujący nagłówek dla funkcji malloc i free
+# include "./ft_so_long.h"
 
 bool ft_dfs(Node *node) 
 {
-    if (node->visited) {
+    if (node->visited)
         return false; // Jeśli węzeł został już odwiedzony, zwróć false
-    } else {
+    else 
         node->visited = true; // Oznacz węzeł jako odwiedzony
-    }
-
-    if (ft_strcmp(node->value, "M") == 0) {
-        return true; // Jeśli wartość węzła to "M", zwróć true
-    } 
-    
-    if (ft_strcmp(node->value, "1") == 0) {
+    if (ft_strcmp(node->value, "M") == 0)
+        return true; // Jeśli wartość węzła to "M", zwróć true 
+    if (ft_strcmp(node->value, "1") == 0)
         return false; // Jeśli wartość węzła to "1", zwróć false
-    }
-
     // Przeszukiwanie sąsiadujących węzłów
     size_t i = 0;
-    while (i < node->neighbor_count) {
+    while (i < node->neighbor_count)
+    {
         bool success = ft_dfs(node->neighbors[i]); // Rekurencyjne wywołanie DFS dla każdego sąsiada
-        if (success) {
+        if (success)
             return true; // Jeśli którykolwiek sąsiad zwrócił true, zwróć true
-        }
         i++;
     }
     return false; // Jeśli żaden z sąsiadów nie zwrócił true, zwróć false
 }
+
+int ft_pre_dfs(char **map, size_t RowAmount)
+{
+    size_t firstRowLength = ft_strlen(map[0]) - 1;
+    Node nodes[RowAmount][firstRowLength]; // Tworzymy macierz węzłów
+    size_t i = 0;
+    while (i < RowAmount) 
+    {
+        size_t j = 0;
+        while (j < firstRowLength) 
+        {
+            nodes[i][j].visited = false; // Ustawiamy początkowy stan odwiedzin na false
+            nodes[i][j].value = &map[i][j]; // Przypisujemy wartość węzła
+            // Tworzymy dla każdego węzła tablicę sąsiadujących węzłów (początkowo pustą)
+            nodes[i][j].neighbors = NULL; // W przypadku braku informacji o sąsiadach
+            nodes[i][j].neighbor_count = 0; // Ustawiamy liczbę sąsiadów na 0
+            j++;
+        }
+        i++;
+    } 
+    // Wyszukujemy węzła startowego (P) na mapie
+     Node *startNode = NULL;
+    i = 0;
+    while (i < RowAmount && startNode == NULL) 
+    {
+        size_t j = 0;
+          //printf("%s\n", nodes[i][j].value);
+         while (j < firstRowLength && startNode == NULL) 
+        {
+           printf("%s\n", nodes[i][j].value);
+            if (strcmp(nodes[i][j].value, "P") == 0)
+                startNode = &nodes[i][j]; 
+            j++;
+        }
+        i++;
+    }
+    
+    // Sprawdzamy, czy znaleziono węzeł startowy
+    if (startNode != NULL) 
+    {
+        bool pathExists = ft_dfs(startNode);
+        if (pathExists)
+            printf("Da się przejść od P do M.\n");
+        else 
+            printf("Nie da się przejść od P do M.\n");
+    }
+    else
+        printf("Nie znaleziono węzła startowego (P) na mapie.\n");
+    return 0; 
+}
+
 int ft_maps_errors(char **map, size_t e)
 {
     size_t firstRowLength = ft_strlen(map[0]); // Długość pierwszego wiersza
     int found_M = 0, found_P = 0;
-
     // Sprawdzenie pierwszego i ostatniego wiersza
     int i = 0;
     while (i < e) 
@@ -79,54 +120,6 @@ int ft_maps_errors(char **map, size_t e)
         return 5; // 'M' lub 'P' nie zostały znalezione w mapie
 
     return 1; // Poprawna rama mapy
-}
-
-int ft_pre_dfs( char **map, size_t RowAmount)
-{
-    size_t firstRowLength = ft_strlen(map[0]) - 1;
-    Node nodes[RowAmount][firstRowLength]; // Tworzymy macierz węzłów
-    size_t i = 0;
-    while (i < RowAmount) 
-    {
-        size_t j = 0;
-        while (j < firstRowLength) 
-        {
-            char *value = &map[i][j]; // Wartość węzła na mapie
-            nodes[i][j].visited = false; // Ustawiamy początkowy stan odwiedzin na false
-            nodes[i][j].value = value; // Przypisujemy wartość węzła
-            // Tworzymy dla każdego węzła tablicę sąsiadujących węzłów (początkowo pustą)
-            nodes[i][j].neighbors = NULL; // W przypadku braku informacji o sąsiadach
-            nodes[i][j].neighbor_count = 0; // Ustawiamy liczbę sąsiadów na 0
-            j++;
-        }
-        i++;
-    }
-    // Wyszukujemy węzła startowego (P) na mapie
-    Node *startNode = NULL;
-    i = 0;
-    while (i < 5 && startNode == NULL) {
-        size_t j = 0;
-        while (j < 5 && startNode == NULL) {
-            if (ft_strcmp(nodes[i][j].value, "P") == 0) {
-                startNode = &nodes[i][j];
-            }
-            j++;
-        }
-        i++;
-    }
-
-    // Sprawdzamy, czy znaleziono węzeł startowy
-    if (startNode != NULL) {
-        bool pathExists = ft_dfs(startNode);
-        if (pathExists) {
-            printf("Da się przejść od P do M.\n");
-        } else {
-            printf("Nie da się przejść od P do M.\n");
-        }
-    } else {
-        printf("Nie znaleziono węzła startowego (P) na mapie.\n");
-    }
-    return 0;
 }
 
 
